b353077452a1e6ea89c22bcf6ea538a05f22c558
Roberto González Téllez <73495438+RobertoGlz@users.noreply.github.com>
2023-12-22 12:43:53 -0600

    Update template_example.js

--- template_example.js
+++ template_example.js
@@ -5,7 +5,7 @@ define(['pipAPI', 'https://robertoglz.github.io/template_main.js'], function(API
 		category1 : {
 			name : 'MORENA', //Will appear in the data.
 			title : {
-				media : {word : 'MORENA'}, //Name of the category presented in the task.
+				media : {image : 'logo_morena.png'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
@@ -23,7 +23,7 @@ define(['pipAPI', 'https://robertoglz.github.io/template_main.js'], function(API
 		category2 :	{
 			name : 'PAN', //Will appear in the data.
 			title : {
-				media : {word : 'PAN'}, //Name of the category presented in the task.
+				media : {image : 'logo_pan.png'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
