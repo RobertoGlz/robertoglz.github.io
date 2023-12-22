b96bbe0dd45c7c712310e5f15232f8179a5bdf9c
Roberto González Téllez <73495438+RobertoGlz@users.noreply.github.com>
2023-12-21 18:40:50 -0600

    Update template_example.js
    Add Roberto's website path to template

--- template_example.js
+++ template_example.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://alerv97.github.io/iat_puebla/iat/template_main.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://robertoglz.github.io/template_main.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
