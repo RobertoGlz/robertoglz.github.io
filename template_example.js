79f9f08933ee854bcd5606e2dc9362789c61f92a
Roberto González Téllez <73495438+RobertoGlz@users.noreply.github.com>
2024-01-04 15:40:15 -0600

    Update template_example.js

--- template_example.js
+++ template_example.js
@@ -3,37 +3,36 @@ define(['pipAPI', 'https://robertoglz.github.io/template_main.js'], function(API
 
 	return iatExtension({
 		category1 : {
-			name : 'MORENA', //Will appear in the data.
+			name : 'Democracia', //Will appear in the data.
 			title : {
-				media: {word : 'MORENA'}, //Name of the category presented in the task.
+				media: {word : 'Democracia'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {word : 'AMLO'}, 
-    			{word : 'Transformación'}, 
-    			{word : 'Izquierda'}, 
-    			{word : 'Cambio'}, 
-    			{word : 'Progresismo'}, 
-    			{word : 'Renovación'}
+    		    {word : 'Democracia'}, 
+    			{word : 'Voto'}, 
+    			{word : 'Democracia de colaboradores'}, 
+    			{word : 'Democracia de socios'}, 
+    			{word : 'Deliberación'},
+			{word : 'Participación'}
 			], 
 			//Stimulus css (style)
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
 		category2 :	{
-			name : 'PAN', //Will appear in the data.
+			name : 'Autoritarismo', //Will appear in the data.
 			title : {
-				media : {word : 'PAN'}, //Name of the category presented in the task.
+				media : {word : 'Autoritarismo'}, //Name of the category presented in the task.
 				css : {color:'#31940F','font-size':'2em'}, //Style of the category title.
 				height : 4 //Used to position the "Or" in the combined block.
 			}, 
 			stimulusMedia : [ //Stimuli content as PIP's media objects
-    		    {word : 'Conservadores'}, 
-    			{word : 'Derecha'}, 
-    			{word : 'Tradición'}, 
-    			{word : 'Empresario'}, 
-    			{word : 'Orden'}, 
-    			{word : 'Alternancia'}			], 
+    		    	{word : 'Autoritarismo'}, 
+    			{word : 'China'}, 
+    			{word : 'Fidel Castro'}, 
+    			{word : 'Anti-democrático'}, 
+    			{word : 'Corea del Norte'}				], 
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
 		},	
